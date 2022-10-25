9ca8ad7fd29e10e153a5ea68e3d92076819faf9f
LuthandoMtengwane <luthandolindokuhle17@gmail.com>
2022-10-25 13:52:41 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../sass/components/navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>Navbar</p>
+    </div>
+  );
+};
+
+export default Navbar;
